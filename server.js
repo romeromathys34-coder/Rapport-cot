5f5f9dc9c3fac7c036ef0bef43694f3f16230f83
romeromathys34-coder <romero.mathys34@gmail.com>
2026-06-08 19:00:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const DATASETS = [
     reportType: 'Futures Only / Non-Commercial',
     groupUsed: 'Non-Commercial',
     formula: 'Position nette = Non-Commercial Long - Non-Commercial Short',
-    url: 'https://www.cftc.gov/files/dea/newcot/deafut.txt',,
+    url: 'https://www.cftc.gov/files/dea/newcot/deafut.txt',
     parser: parseLegacyRow
   },
   {
@@ -27,7 +27,7 @@ const DATASETS = [
     reportType: 'Futures Only / Managed Money',
     groupUsed: 'Managed Money',
     formula: 'Position nette = Managed Money Long - Managed Money Short',
-    url: 'https://www.cftc.gov/files/dea/newcot/f_disagg.txt',,
+    url: 'https://www.cftc.gov/files/dea/newcot/f_disagg.txt',
     parser: parseDisaggregatedRow
   },
   {
